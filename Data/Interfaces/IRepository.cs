c2a380c09ea5cd6ca05ef1be3804cf7b77223210
Suleyman GUMUS <suleyman_gumus@outlook.com.tr>
2024-10-22 02:09:42 +0300

    repos added

---
+++ Data/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace task_management_tekhnelogos.Data.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task InsertAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Data/Repository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using task_management_tekhnelogos.Data.Interfaces;
+namespace task_management_tekhnelogos.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this.dbSet = context.Set<T>();
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await dbSet.ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+        public async Task InsertAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+        }
+        public void Update(T entity)
+        {
+            dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+        public void Delete(T entity)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+            {
+                dbSet.Attach(entity);
+            }
+            dbSet.Remove(entity);
+        }
+    }
+}
