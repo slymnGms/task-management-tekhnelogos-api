f11a93507afd010b3d739e0f72df20b9b381f5e3
Suleyman GUMUS <suleyman_gumus@outlook.com.tr>
2024-10-22 02:13:12 +0300

    controllers added

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using task_management_tekhnelogos.Services.Interfaces;
+using task_management_tekhnelogos.Services.Models.DTO;
+namespace task_management_tekhnelogos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class TasksController(ITaskService taskService) : ControllerBase
+    {
+        private readonly ITaskService _taskService = taskService;
+        [HttpPost]
+        public async Task<IActionResult> CreateTask([FromBody] TaskDto taskDto)
+        {
+            var task = await _taskService.CreateTaskAsync(taskDto);
+            return Ok(task);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetTasks()
+        {
+            var tasks = await _taskService.GetTasksAsync();
+            return Ok(tasks);
+        }
+        [HttpPost("assign")]
+        public async Task<IActionResult> AssignTasksToUsers([FromBody] AssignTasksDto assignTasksDto)
+        {
+            try
+            {
+                await _taskService.AssignTasksToUsersAsync(assignTasksDto.TaskIds, assignTasksDto.UserIds);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("mytasks")]
+        public async Task<IActionResult> GetMyTasks()
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var tasks = await _taskService.GetTasksByUserIdAsync(userId);
+            return Ok(tasks);
+        }
+        [HttpGet("tasks-by-users")]
+        public async Task<IActionResult> GetTasksGroupedByUsers()
+        {
+            var tasksWithUsers = await _taskService.GetTasksGroupedByUsersAsync();
+            return Ok(tasksWithUsers);
+        }
+        [HttpGet("attended-tasks")]
+        public async Task<IActionResult> GetAttendedTasks()
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var tasks = await _taskService.GetAttendedTasksByUserIdAsync(userId);
+            return Ok(tasks);
+        }
+        [HttpGet("assignments")]
+        public async Task<IActionResult> GetAllAssignments()
+        {
+            var assignments = await _taskService.GetAllAssignmentsAsync();
+            return Ok(assignments);
+        }
+        [HttpDelete("assignments/{userId}/{taskId}")]
+        public async Task<IActionResult> DeleteUserTaskAssignment(int userId, int taskId)
+        {
+            try
+            {
+                await _taskService.DeleteUserTaskAsync(userId, taskId);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using task_management_tekhnelogos.Services.Interfaces;
+using task_management_tekhnelogos.Services.Models.DTO;
+namespace task_management_tekhnelogos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(IUserService userService) : ControllerBase
+    {
+        private readonly IUserService _userService = userService;
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto userDto)
+        {
+            var user = await _userService.RegisterAsync(userDto);
+            return Ok(user);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var token = await _userService.LoginAsync(loginDto);
+            if (token == null) return Unauthorized();
+            return Ok(new { Token = token });
+        }
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+    }
+}
