c7e392e7117dec31efeeff334bb178fd44adc830
Suleyman GUMUS <suleyman_gumus@outlook.com.tr>
2024-10-22 02:10:53 +0300

    mapping added

---
+++ Services/Mappers/GeneralMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using task_management_tekhnelogos.Data.Models;
+using task_management_tekhnelogos.Services.Models.DTO;
+
+namespace task_management_tekhnelogos.Services.Mappers
+{
+    public class GeneralMappingProfile : Profile
+    {
+        public GeneralMappingProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<TaskItem, TaskDto>().ReverseMap();
+            CreateMap<UserTask, UserTaskDto>();
+        }
+    }
+}
